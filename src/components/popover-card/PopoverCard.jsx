adfc9d152766c59b520696a5c894c45244578cb1
Ahmed Gamal <ahmedgamal.ssaleh@gmail.com>
2022-09-09 17:13:26 +0200

    feat: Finish popover card styling

--- src/components/popover-card/PopoverCard.jsx
+++ src/components/popover-card/PopoverCard.jsx
@@ -1,19 +1,42 @@
-import React from 'react';
-import styles from './PopoverCard.module.css'
-import months from '../../months';
+import React from "react";
+import styles from "./PopoverCard.module.css";
+import months from "../../months";
 
-function PopoverCard({course}) {
-    const [year, month] = course.last_update_date.split('-');
-    const date = new Date(year, month - 1);
-    
-    return (
-        <div className={styles.cardWrapper}>
-            <a className={styles.courseTitle} href="">
-                <p>{course.title}</p>
-            </a>
-            <p className={styles.date}>Updated <span>{months[date.getMonth()]} {date.getFullYear()}</span></p>
-        </div>
-     );
+function PopoverCard({ course }) {
+  const [year, month] = course.last_update_date.split("-");
+  const date = new Date(year, month - 1);
+
+  const objectives = course.objectives_summary.map((objective) => (
+    <p>
+      <span className={styles.tickIcon}>
+        <i className="fa-solid fa-check"></i>
+      </span>
+      {objective}
+    </p>
+  ));
+
+  return (
+    <div className={styles.cardWrapper}>
+      <a className={styles.courseTitle} href="#">
+        <p>{course.title}</p>
+      </a>
+      <p className={styles.date}>
+        Updated{" "}
+        <span>
+          {months[date.getMonth()]} {date.getFullYear()}
+        </span>
+      </p>
+      <p className={styles.info}>
+        {course.content_info} . {course.instructional_level} . Subtitles
+      </p>
+      <p>{course.headline}</p>
+      {objectives}
+      <div className={styles.buttonsSection}>
+        <button className={styles.addToCart}>Add to cart</button>
+        <button className={styles.heartButton}><i class="fa-regular fa-heart"></i></button>
+      </div>
+    </div>
+  );
 }
 
 export default PopoverCard;
