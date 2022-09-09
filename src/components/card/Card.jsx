cfb373707373cf1490b003083b31bb3e5adbc71f
Ahmed Gamal <ahmedgamal.ssaleh@gmail.com>
2022-09-09 15:28:12 +0200

    feat: Add a popover to card component

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './Card.module.css'
+import { Popover } from '@mui/material';
 
 const Card = ({course}) => {
     const {headline:title, visible_instructors, image_304x171:image, rating} = course;
     const instructors = visible_instructors.map(instructor => instructor.title).join(', ');
+
+    const [anchorEl, setAnchorEl] = useState(null);
+
+    
+    const openPopover = (event) => {
+        setAnchorEl(event.currentTarget);
+    }
+
+    const closePopover = () => {
+        setAnchorEl(null);
+    }
+    const open = Boolean(anchorEl);
+
     return (
         <>
-            <div className={styles.courseWrapper}>
+            <div 
+                aria-owns={open ? 'mouse-over-popover' : undefined}
+                aria-haspopup="true" 
+                onMouseEnter={openPopover} 
+                onMouseLeave={closePopover} 
+                className={styles.courseWrapper}>
                 <article className={styles.card}>
                     <figure className={styles.wrapper}>
                         <img src={image} alt="" />
@@ -30,6 +49,36 @@ const Card = ({course}) => {
                     </section>
                 </article>
             </div>
+
+            <Popover 
+                id="mouse-over-popover"
+                open={open}
+                anchorEl={anchorEl}
+                onClose={closePopover}
+                onMouseLeave={closePopover}
+                anchorOrigin={{
+                    vertical: 'center',
+                    horizontal: 'center',
+                }}
+                transformOrigin={{
+                    vertical: 'center',
+                    horizontal: 'center',
+                }}
+                // disableRestoreFocus
+                sx={{
+                    pointerEvents: 'none'
+                }}
+                PaperProps={{
+                    onMouseEnter: openPopover,
+                    onMouseLeave: closePopover,
+                    sx: {
+                        pointerEvents: 'auto'
+                    }
+                }}
+                >
+                
+            </Popover>
+
         </>
     );
 }
--- src/components/courses-container/CoursesContainer.jsx
+++ src/components/courses-container/CoursesContainer.jsx
@@ -2,17 +2,23 @@ import React, {useContext} from 'react'
 import Card from '../card/Card';
 import styles from './CoursesContainer.module.css'
 import { SearchTermContext } from '../../App';
+import { Popover } from '@mui/material';
 
 const CoursesContainer = ({courses}) => {
+
     const searchTerm = useContext(SearchTermContext).toLowerCase();
 
     const coursesCards = courses.filter(course => course.headline.toLowerCase().includes(searchTerm)).map((course) => {
         return <Card key={course.id} course={course}></Card>;
     });
+
+
     return (
-        <section className={styles.container}>
-            {coursesCards.length ? coursesCards : <p className={styles.emptyCoursesList}>There're no courses to show</p>}
-        </section>
+        <>
+            <section className={styles.container}>
+                {coursesCards.length ? coursesCards : <p className={styles.emptyCoursesList}>There're no courses to show</p>}
+            </section>
+        </>
     );
 }
 
