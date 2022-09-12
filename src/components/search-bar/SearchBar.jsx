7d7e1919f15c097d5105b888a85ce0954211e4a7
Ahmed Gamal <ahmedgamal.ssaleh@gmail.com>
2022-09-12 15:58:43 +0200

    feat: Add search with Enter key to search bar

--- src/components/search-bar/SearchBar.jsx
+++ src/components/search-bar/SearchBar.jsx
@@ -7,23 +7,29 @@ function SearchBar() {
 
     const [barValue, setBarValue] = useState('');
 
-    const update = (e) => {
-        setBarValue(e.target.value);
+    const update = e => {
         e.preventDefault();
+        setBarValue(e.target.value);
     }
 
     const searchPressed = (barValue) => {
         setSearchTerm(barValue);
         const coursesSection = document.querySelector('.courses-section');
-        console.log(coursesSection);
-        coursesSection.scrollIntoView({behavior:"smooth", block:'start', inline:'nearest'});
+        coursesSection.scrollIntoView({behavior:"smooth", block:'start'});
+    }
+
+    const keySearch = e => {
+        if(e.key === 'Enter'){
+            e.preventDefault();
+            searchPressed(barValue);
+        }
     }
 
     return (         
         <li className="search-bar">
             <form action=''>
-                <button className="search-icon" type="button"><i className="fa-solid fa-magnifying-glass" onClick={() => searchPressed(barValue)}></i></button>
-                <input type="text" placeholder="Search for anything" className="bar" onChange={update} />
+                <button onClick={() => searchPressed(barValue)} type="button" className="search-icon"><i className="fa-solid fa-magnifying-glass"></i></button>
+                <input onKeyDown={keySearch} type="text" placeholder="Search for anything" className="bar" onChange={update} />
             </form>
         </li>
      );
