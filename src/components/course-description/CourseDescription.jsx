acadfbd875b63d4933a8d0e47f2b1d596c83f45d
Ahmed Gamal <ahmedgamal.ssaleh@gmail.com>
2022-09-17 20:51:25 +0200

    feat: Add course description section

--- src/components/course-description/CourseDescription.jsx
+++ src/components/course-description/CourseDescription.jsx
@@ -1,25 +1,12 @@
 import React from 'react';
 import styles from './CourseDescription.module.css';
-import HTMLReactParser from 'html-react-parser';
+import FadedText from '../faded-text/FadedText';
 
 function CourseDescription({ details }) {
-	let text = details['description'];
-	text = text
-		.replace(/<ul>/g, `<div class="${styles.pointsCollection}">`)
-		.replace(/<\/ul>/g, '</div>')
-		.replace(
-			/<li><p>/g,
-			`<p class=${styles.point}><i class="fa-solid fa-circle ${styles.dotIcon}"></i>`
-		)
-		.replace(/<\/p><\/li>/g, '</p>');
 	return (
 		<div className={styles.mainContainer}>
-            <p className={styles.header}>
-                Description
-            </p>
-			<div className={styles.descriptionContent}>
-				{HTMLReactParser(text)}
-			</div>
+			<p className={styles.header}>Description</p>
+			<FadedText rawHTML={details['description']} />
 		</div>
 	);
 }
