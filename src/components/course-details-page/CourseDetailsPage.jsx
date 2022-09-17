4625dad7ea1deb0066220dad7fa1c099c4f34e67
Ahmed Gamal <ahmedgamal.ssaleh@gmail.com>
2022-09-17 23:23:21 +0200

    feat: Add "this course includes" section to the single course page

--- src/components/course-details-page/CourseDetailsPage.jsx
+++ src/components/course-details-page/CourseDetailsPage.jsx
@@ -8,6 +8,7 @@ import CourseRequirements from '../course-requirements/CourseRequirements';
 import styles from './CourseDetailsPage.module.css';
 import CourseDescription from '../course-description/CourseDescription';
 import InstructorsSection from '../../instructors-section/InstructorsSection';
+import ThisCourseIncludes from '../this-course-includes/ThisCourseIncludes';
 
 function CourseDetailsPage({ courseDetails }) {
 	const [fetched, setAsFetched] = useState(false);
@@ -28,6 +29,9 @@ function CourseDetailsPage({ courseDetails }) {
 				<SingleCourseHeader courseDetails={courseDetails} />
 				<div className={styles.body}>
 					<CourseObjectives courseDetails={courseDetails} />
+					<div className={styles.includesSection}>
+						<ThisCourseIncludes details={additionalDetails} />
+					</div>
 					<CourseContent details={additionalDetails} />
 					<CourseRequirements details={additionalDetails} />
 					<CourseDescription details={additionalDetails} />
---
+++ src/components/this-course-includes/ThisCourseIncludes.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import styles from './ThisCourseIncludes.module.css';
+import {
+	OndemandVideo,
+	InsertDriveFile,
+	Download,
+	AllInclusive,
+	StayCurrentPortrait,
+	EmojiEvents,
+} from '@mui/icons-material';
+import { List, ListItem, ListItemIcon, ListItemText } from '@mui/material';
+
+function ThisCourseIncludes({ details }) {
+	const {
+		content_length_video: seconds,
+		num_additional_assets: downloadable,
+	} = details;
+
+	const icons = [
+		<OndemandVideo className={styles.black} />,
+		<InsertDriveFile className={styles.black} />,
+		<Download className={styles.black} />,
+		<AllInclusive className={styles.black} />,
+		<StayCurrentPortrait className={styles.black} />,
+		<EmojiEvents className={styles.black} />,
+	];
+	const itemsText = [
+		`${parseInt(seconds / 60 / 60)} hours on-demand video`,
+		'1 article',
+		`${downloadable} downloadable resources`,
+		'Full lifetime access',
+		'Access on mobile and TV',
+		'Certificate of completion',
+	];
+
+	return (
+		<main className={styles.mainContainer}>
+			<p className={styles.header}>This course includes:</p>
+			<List>
+				{icons.map((icon, idx) => {
+					return (
+						<ListItem
+							key={idx}
+							disablePadding
+							className={styles.listItem}
+						>
+							<ListItemIcon>{icon}</ListItemIcon>
+							<ListItemText className={styles.itemText}>
+								<span className={styles.black}>
+									{itemsText[idx]}
+								</span>
+							</ListItemText>
+						</ListItem>
+					);
+				})}
+			</List>
+		</main>
+	);
+}
+
+export default ThisCourseIncludes;
